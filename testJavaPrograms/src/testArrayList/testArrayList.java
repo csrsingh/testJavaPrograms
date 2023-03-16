f52eea0907aa5e0fc50f7f1ead2737e449a42b2c
Chandrashekhar Singh <cssingh.sv@gmail.com>
2023-03-16 16:06:58 +0530

    Update testArrayList.java
    First update from the github

--- testJavaPrograms/src/testArrayList/testArrayList.java
+++ testJavaPrograms/src/testArrayList/testArrayList.java
@@ -7,8 +7,8 @@ public class testArrayList {
 
 	public static void main(String[] args) {
 		// ArrayList = a resizable array
-				//				Elements can be added and removed after compilation phase
-				//				store reference data types
+				//Elements can be added and removed after compilation phase
+				//store reference data types
 				ArrayList<String> food = new ArrayList<String>();
 				
 				food.add("Pitza");
@@ -16,10 +16,10 @@ public class testArrayList {
 				food.add("hotdog");
 				
 				food.set(0, "Sushi");
-				//food.remove(2);
-				//food.clear();
+				food.remove(2);
+				food.clear();
 				for(int i=0; i<food.size(); i++) {
-					System.out.println(food.get(i));
+				System.out.println(food.get(i));
 				}
 		
 
