d02162a4cffa902a32f2d1c263a33a21dedfb324
Chandrashekhar Singh <cssingh.sv@gmail.com>
2023-03-17 00:27:27 +0530

    test Array List

--- testArrayList/src/testArrayList/testArrayList.java
+++ testArrayList/src/testArrayList/testArrayList.java
@@ -5,20 +5,18 @@ public class testArrayList {
 
 	public static void main(String[] args) {
 			// ArrayList = a re-sizable array
-					//Elements can be added and removed after compilation phase
-					//store reference data types
+				//Elements can be added and removed after compilation phase
+				//store reference data types
 			ArrayList<String> food = new ArrayList<String>();				
 			food.add("Pitza");
 			food.add("Hamburger");
-			food.add("hotdog");	
-						
-			//food.set(0,"Sushi");
+			food.add("hotdog");						
+			//food.set(0,"sushi");
 			//food.remove(2);
-			//food.clear();
-			
+			//food.clear();			
 			for(int i=0; i<food.size(); i++) {
 			System.out.println(food.get(i));
-			}
+		}
 	}
 
 }
